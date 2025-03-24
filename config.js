deb2ab947154a5fb9089a96383b3143808c7482c
bobbiswas69 <bob69@duck.com>
2025-03-23 21:27:49 -0400

    changing file structure

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ const config = {
     googleClientId: 'your_development_client_id'
   },
   production: {
-    apiUrl: 'http://localhost:3000',
-    googleClientId: 'your_production_client_id'
+    apiUrl: 'http://localhost:3000', // We'll update this after backend deployment
+    googleClientId: 'your_production_client_id' // We'll get this from Google Cloud Console
   }
 };
 
 const environment = window.location.hostname === 'localhost' ? 'development' : 'production';
-export default config[environment]; 
+export default config[environment];
